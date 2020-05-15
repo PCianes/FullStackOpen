7ec5e87da848beead006a57ea529c7b8b391d20a
Pablo Cianes <cianespablo@gmail.com>
2020-05-15 16:34:51 +0200

    🚧 2.8: The Phonebook Step

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "999-778866" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const onFormSubmit = (e) => {
     e.preventDefault();
     setNewName("");
+    setNewNumber("");
     const alreadyExists = persons.some((person) => person.name === newName);
     if (alreadyExists) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
   };
 
   return (
@@ -20,8 +24,19 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={onFormSubmit}>
         <div>
-          name:{" "}
-          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          name:
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            autoFocus
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -29,7 +44,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} <span>{person.number}</span>
+        </p>
       ))}
     </div>
   );
