bd598f1e6d2554913218f7102f15a85ae3dd34d5
Pablo Cianes <cianespablo@gmail.com>
2020-05-15 12:47:14 +0200

    ✨ 2.5: separate module

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,54 +1,7 @@
 import React from "react";
 import "./App.css";
-import Course from "./components/Course";
+import Courses from "./Courses";
 
-const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-
-  return courses.map((course) => <Course key={course.id} course={course} />);
-};
+const App = () => <Courses />;
 
 export default App;
