216bbdc232ad91aad25cc03c0efc080272f90124
Pablo Cianes <cianespablo@gmail.com>
2020-05-19 10:27:17 +0200

    🎉 3.11 phonebook full stack

---
+++ part3/phonebook/build/precache-manifest.364c49d967eb936fd1413b38b6b6e821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce80220f060d649ab40f415620d2de94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2da711b3432284859ed6",
+    "url": "/static/css/main.2989f2ef.chunk.css"
+  },
+  {
+    "revision": "a75cfe734ddb11b2e053",
+    "url": "/static/js/2.2381a9e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2381a9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da711b3432284859ed6",
+    "url": "/static/js/main.433d9d8a.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
