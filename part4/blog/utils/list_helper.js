4a99cfc52742fa5b85f87d7604430c1268a0f0c7
Pablo Cianes <cianespablo@gmail.com>
2020-05-22 13:32:13 +0200

    🚧 4.4: helper functions and unit tests, step

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (!Array.isArray(blogs) || !blogs.length) return 0
+
+  if (blogs.length === 1) return blogs[0].likes
+
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
