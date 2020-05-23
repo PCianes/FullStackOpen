9b7f3c266666c29d26013f0c658e7e84a7c99b7e
Pablo Cianes <cianespablo@gmail.com>
2020-05-23 11:40:21 +0200

    🚧 4.15: bloglist expansion, step3

---
+++ part4/blog/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
