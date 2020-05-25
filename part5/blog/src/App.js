60ccbf209ca7ae3c74fa9b5ea35c9f55cb09d75a
Pablo Cianes <cianespablo@gmail.com>
2020-05-25 11:56:08 +0200

    🚧 5.9*: Blog list frontend, step9

--- part5/blog/src/App.js
+++ part5/blog/src/App.js
@@ -125,9 +125,11 @@ const App = () => {
           </Togglable>
           <hr></hr>
           <div>
-            {blogs.map((blog) => (
-              <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-            ))}
+            {blogs
+              .sort((a, b) => b.likes - a.likes)
+              .map((blog) => (
+                <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+              ))}
           </div>
         </>
       )}
