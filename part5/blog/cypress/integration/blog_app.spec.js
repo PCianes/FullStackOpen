987e8795a2c1e1b1eb1a3904ec9d70bdd4e59343
Pablo Cianes <cianespablo@gmail.com>
2020-05-26 16:19:03 +0200

    🚧 5.18: bloglist end to end testing, step2

--- part5/blog/cypress/integration/blog_app.spec.js
+++ part5/blog/cypress/integration/blog_app.spec.js
@@ -1,10 +1,37 @@
 describe('Blog app', function () {
+  const userTest = {
+    username: 'admin',
+    name: 'Admin',
+    password: 'admin1234',
+  }
+
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', userTest)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function () {
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type(userTest.username)
+      cy.get('#password').type(userTest.password)
+      cy.get('#login-button').click()
+
+      cy.get('.notification--success').contains(`Welcome ${userTest.name}`)
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type(userTest.username)
+      cy.get('#password').type('wrong password')
+      cy.get('#login-button').click()
+
+      cy.get('.notification--error')
+        .contains('wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- part5/blog/src/components/LoginForm.js
+++ part5/blog/src/components/LoginForm.js
@@ -17,6 +17,7 @@ const LoginForm = ({ handleLogin }) => {
       <div>
         username
         <input
+          id="username"
           type="text"
           value={username}
           name="Username"
@@ -26,13 +27,16 @@ const LoginForm = ({ handleLogin }) => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">
+        login
+      </button>
     </form>
   )
 }
