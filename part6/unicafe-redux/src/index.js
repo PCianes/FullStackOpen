f88ce07c991902851ee6d6177cd394019b75ecbc
Pablo Cianes <cianespablo@gmail.com>
2020-05-27 11:08:11 +0200

    ✨6.2: unicafe revisited, step2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -6,21 +6,24 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const handleDispatch = (type) => store.dispatch({ type })
+
+  const good = () => handleDispatch('GOOD')
+  const neutral = () => handleDispatch('OK')
+  const bad = () => handleDispatch('BAD')
+  const reset = () => handleDispatch('ZERO')
+
+  const getStore = (item) => store.getState()[item]
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={good}>good</button>
+      <button onClick={neutral}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {getStore('good')}</div>
+      <div>neutral {getStore('ok')}</div>
+      <div>bad {getStore('bad')}</div>
     </div>
   )
 }
