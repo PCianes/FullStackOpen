f9111a3badd3edbbb6c5f39c06d10bca64b06748
Pablo Cianes <cianespablo@gmail.com>
2020-05-30 12:46:54 +0200

    ⚙ fix initial bug with empty useEffect()

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -11,14 +11,14 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  //useEffect()
 
   return country
 }
@@ -29,19 +29,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
