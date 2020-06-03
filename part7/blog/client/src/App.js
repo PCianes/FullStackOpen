4b667c81a12383af17082cd792c994752dddc7a7
Pablo Cianes <cianespablo@gmail.com>
2020-06-03 17:12:35 +0200

    🎉 7.13: Users view: add React-Router

--- part7/blog/client/src/App.js
+++ part7/blog/client/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { initializeBlogs, addBlog } from './reducers/blogsReducer'
 import { setSuccessMessage, setErrorMessage } from './reducers/messageReducer'
 import { setUser, userLogout } from './reducers/userReducer'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import Users from './components/Users'
 import Blog from './components/Blog'
@@ -42,32 +43,49 @@ const App = () => {
     dispatch(userLogout())
   }
 
+  const padding = { padding: 5 }
+
   return (
     <div>
-      <h2>blogs</h2>
-      <Notification message={message} />
-      {user === null ? (
-        <LoginForm />
-      ) : (
-        <>
-          <p>
-            {user.name} logged in <button onClick={handleLogout}>Logout</button>
-          </p>
-          <Togglable buttonLabel="new blog" ref={blogFormRef}>
-            <h2>create new</h2>
-            <BlogForm handleBlogForm={handleBlogForm} />
-          </Togglable>
-          <hr></hr>
-          <div>
-            {blogs
-              .sort((a, b) => b.likes - a.likes)
-              .map((blog) => (
-                <Blog key={blog.id} blog={blog} />
-              ))}
-          </div>
-        </>
-      )}
-      <Users />
+      <Router>
+        <Link style={padding} to="/">
+          Blogs
+        </Link>
+        <Link style={padding} to="/users">
+          Users
+        </Link>
+        <Notification message={message} />
+        <Switch>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <h2>blogs</h2>
+            {user === null ? (
+              <LoginForm />
+            ) : (
+              <>
+                <p>
+                  {user.name} logged in{' '}
+                  <button onClick={handleLogout}>Logout</button>
+                </p>
+                <Togglable buttonLabel="new blog" ref={blogFormRef}>
+                  <h2>create new</h2>
+                  <BlogForm handleBlogForm={handleBlogForm} />
+                </Togglable>
+                <hr></hr>
+                <div>
+                  {blogs
+                    .sort((a, b) => b.likes - a.likes)
+                    .map((blog) => (
+                      <Blog key={blog.id} blog={blog} />
+                    ))}
+                </div>
+              </>
+            )}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
