b164e04f1c161916a403f01b0723d1eb915de5ec
Pablo Cianes <cianespablo@gmail.com>
2020-06-24 12:31:09 +0200

    🚧 9.8: Patientor backend, step1

---
+++ part9/patientor/server/build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use(express_1.default.json());
+const PORT = 3000;
+app.get('/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
