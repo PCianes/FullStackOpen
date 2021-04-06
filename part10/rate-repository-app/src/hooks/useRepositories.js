0be712a0e99521390e0e5b18d45ebbf855e5aaf5
Pablo Cianes <cianespablo@gmail.com>
2021-04-06 13:52:53 +0200

    ✨ get data from API with fetch and custom hook

---
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const response = await fetch('http://192.168.1.37:5000/api/repositories');
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
